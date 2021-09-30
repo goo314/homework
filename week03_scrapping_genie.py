fc9b10f8ed777e534ec8d3fdc7740351100317f6
goo314 <rn0731@naver.com>
2021-10-01 04:15:39 +0900

    Update week03_scrapping_genie.py

--- week03_scrapping_genie.py
+++ week03_scrapping_genie.py
@@ -4,7 +4,6 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 # 아래 빈 칸('')을 채워보세요
@@ -18,9 +17,11 @@ for tr in trs:
     rank = tr.select_one('td.number').text[0:2].strip()
     title = tr.select_one('td.info > a.title.ellipsis').text.strip()
     artist = tr.select_one('td.info > a.artist.ellipsis').text
+    print(rank, title, artist)
 
     music = {
         'rank': rank,
         'title': title,
-        'artist': artist}
+        'artist': artist
+    }
     db.musics.insert_one(music)
